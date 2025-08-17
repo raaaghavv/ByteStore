f801a1229641ee944efc06c413f19066e6f5e958
raaaghavv <raghavgoel454@gmail.com>
2025-08-17 18:54:34 +0530

    fix: wrap homePage with filter provider separatly

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import { Suspense } from "react";
 import HomePage from "@/components/HomePage";
+import { FilterProvider } from "@/context/FilterContext";
 
 export default function Home() {
   return (
     <Suspense
       fallback={<div className="text-center p-8">Loading products...</div>}
     >
-      <HomePage />
+      <FilterProvider>
+        <HomePage />
+      </FilterProvider>
     </Suspense>
   );
 }
