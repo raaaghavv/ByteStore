10b52e679516b85edeff40fb82e1014bf8d77654
raaaghavv <raghavgoel454@gmail.com>
2025-08-17 14:54:43 +0530

    feat: use Link tag to navigate the user to cart page and add total quantity display on the cart button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,34 @@
+"use client";
+import Link from "next/link";
 import React from "react";
+import { useCart } from "@/context/CartContext";
 
 const Header = () => {
+  const { cartItems } = useCart();
+  const totalItems = cartItems.reduce((acc, item) => acc + item.quantity, 0);
   return (
     <header className="sticky top-0 flex items-center justify-center bg-brand w-full z-90">
       <div className="flex items-center justify-between max-w-[1440px] w-full mx-4 sm:mx-6 py-4">
-        <div className="text-white text-3xl font-bold px-3 py-1">Logo</div>
+        <Link href={"/"} className="text-white text-3xl font-bold px-3 py-1">
+          Logo
+        </Link>
 
         <input
           type="text"
           placeholder="Search for products..."
           className="min-w-25 w-5/12 px-2 py-2.5 rounded-lg border border-white/70 text-white text-sm bg-transparent focus:outline-none"
         />
-        <button className="bg-accent text-white px-4 sm:px-6 py-2.5 rounded flex items-center gap-1 whitespace-nowrap">
+        <Link
+          href={"/cart"}
+          className="bg-accent text-white px-4 sm:px-6 py-2.5 rounded flex items-center gap-1 whitespace-nowrap"
+        >
           🛒 Cart
-        </button>
+          <sup>
+            <span className=" bg-red-500 text-white text-center rounded-full aspect-square px-1.5 py-1">
+              {totalItems > 0 ? totalItems : ""}
+            </span>
+          </sup>
+        </Link>
       </div>
     </header>
   );
